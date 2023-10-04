5ab3b138e73c733673c7404dbcbdd9acff0fc2ae
gareth.james <gareth.james@vodafone.com>
2023-10-04 17:08:12 +0100

    Removed footer ad

--- client/src/components/PageFooter.jsx
+++ client/src/components/PageFooter.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import AdFooter from '@components/AdFooter';
-
 function PageFooter() {
   return (
     <div className="page-footer container">
       <div className="page-footer__content">
-        <AdFooter />
+        &copy; 2023
       </div>
     </div>
   );
