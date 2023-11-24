bf2732be5ed237158ea106cb8628bea8f447edfc
gareth.james <gareth.james@vodafone.com>
2023-11-24 10:35:07 +0000

    Fixed maps errors

--- server/data/mapAccessibility.js
+++ server/data/mapAccessibility.js
@@ -8,6 +8,10 @@ const mapAccessibility = {
           access: 'platform',
         },
         {
+          id: '940GZZLUPAC', // Paddington
+          access: 'platform',
+        },
+        {
           id: '940GZZLUWYC', // Wembley Central
           access: 'platform',
         },
@@ -15,11 +19,63 @@ const mapAccessibility = {
           id: '940GZZLUWJN', // Willesden Junction
           access: 'platform',
         },
-        {
-          id: '940GZZLUPAC', // Paddington
-          access: 'platform',
-        },
       ],
+    },
+    {
+      id: 'central',
+      stations: [],
+    },
+    {
+      id: 'circle',
+      stations: [],
+    },
+    {
+      id: 'district',
+      stations: [],
+    },
+    {
+      id: 'hammersmith-city',
+      stations: [],
+    },
+    {
+      id: 'jubilee',
+      stations: [],
+    },
+    {
+      id: 'metropolitan',
+      stations: [],
+    },
+    {
+      id: 'northern',
+      stations: [],
+    },
+    {
+      id: 'piccadilly',
+      stations: [],
+    },
+    {
+      id: 'victoria',
+      stations: [],
+    },
+    {
+      id: 'waterloo-city',
+      stations: [],
+    },
+    {
+      id: 'london-overground',
+      stations: [],
+    },
+    {
+      id: 'elizabeth',
+      stations: [],
+    },
+    {
+      id: 'dlr',
+      stations: [],
+    },
+    {
+      id: 'tram',
+      stations: [],
     },
   ],
 };
