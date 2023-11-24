f5a582114a1b29591392c26c0515d65aeba5f565
gareth.james <gareth.james@vodafone.com>
2023-11-24 10:37:18 +0000

    Fixed bus and national rail maps

--- server/utils/getStationAccessibility.js
+++ server/utils/getStationAccessibility.js
@@ -2,6 +2,7 @@ import mapAccessibility from '../data/mapAccessibility.js';
 
 const getStationAccessibility = ({ id, naptanId }) => {
   const service = mapAccessibility.services.find((service) => service.id === id);
+  if (!service) return null;
   const station = service.stations.find((station) => station.id === naptanId);
   return station ? station.access : null;
 };
